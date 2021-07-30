b93f21e37079211e23c57854dea77762ec3633f7
kaiyu-tech <kaiyu326@gmail.com>
2021-07-30 22:05:31 +0900

    if文を後置if文に修正しました。

--- 01.fizzbuzz/fizzbuzz.rb
+++ 01.fizzbuzz/fizzbuzz.rb
@@ -2,18 +2,10 @@
 
 1.upto(20) do |i|
   result = ""
-  
-  if i % 3 == 0
-    result += "Fizz"
-  end
-  
-  if i % 5 == 0
-    result += "Buzz"
-  end
-  
-  if result.empty?
-    result = i
-  end
+
+  result += "Fizz" if i % 3 == 0
+  result += "Buzz" if i % 5 == 0
+  result = i if result.empty?
   
   puts result
 end
