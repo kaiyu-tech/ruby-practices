f7694752981c73264f8a3d0d4e13454cbb6d0946
kaiyu-tech <kaiyu326@gmail.com>
2021-08-06 20:47:05 +0900

    マジックナンバーの定数化、可読性の向上について修正しました。

--- 05.ls/ls.rb
+++ 05.ls/ls.rb
@@ -18,17 +18,17 @@ end
 
 # ファイルのアクセス権
 def file_access_char(value, is_special, special_char)
-  result = +''
+  file_access = +''
 
-  result << (value[0] == '1' ? 'r' : '-')
-  result << (value[1] == '1' ? 'w' : '-')
-  result << if value[2] == '1'
-              is_special ? special_char : 'x'
-            else
-              is_special ? special_char.upcase : '-'
-            end
+  file_access << (value[0] == '1' ? 'r' : '-')
+  file_access << (value[1] == '1' ? 'w' : '-')
+  file_access << if value[2] == '1'
+                   is_special ? special_char : 'x'
+                 else
+                   is_special ? special_char.upcase : '-'
+                 end
 
-  result
+  file_access
 end
 
 # 通常表示
@@ -41,76 +41,87 @@ def path_normal(path_list)
 
   max_length = path_list.max_by(&:length).length
 
-  result = +''
+  files = +''
   path_list.each_slice(rows).to_a.transpose.flatten.each_with_index do |path, i|
     next if path.empty?
 
-    result << path.ljust(max_length + 2)
-    result << "\n" if i % cols == cols - 1
+    files << path.ljust(max_length + 2)
+    files << "\n" if i % cols == cols - 1
   end
 
-  result
+  files
 end
 
 # 詳細表示のためのデータを生成
-def generate_parts(path, total_blocks, cols)
+FILE_LIST_COLS = 6
+FILE_MODE = 0
+FILE_NUMBER_OF_HARD_LINKS = 1
+FILE_USER_NAME_OF_OWNER = 2
+FILE_GROUP_NAME_OF_OWNER = 3
+FILE_FILE_BYTE_SIZE = 4
+FILE_LAST_MODIFIED_TIME = 5
+def generate_parts(path)
+  cols = Array.new(FILE_LIST_COLS) { +'' }
+
   status = File.lstat(path)
 
   file_type = status.mode.to_s(8).chars[0..-5].join
   special_type = status.mode.to_s(8).chars[-4]
   access_type = status.mode.to_s(2).chars[-9..-1].join
 
-  cols[0] << (file_type_char(file_type.to_i(8)) +
+  cols[FILE_MODE] << (file_type_char(file_type.to_i(8)) +
   file_access_char(access_type[0..2], special_type == '4', 's') +
   file_access_char(access_type[3..5], special_type == '2', 's') +
   file_access_char(access_type[6..8], special_type == '1', 't'))
 
-  cols[1] << status.nlink.to_s
+  cols[FILE_NUMBER_OF_HARD_LINKS] << status.nlink.to_s
 
-  cols[2] << "#{Etc.getpwuid(status.uid).name}  #{Etc.getgrgid(status.gid).name}"
+  cols[FILE_USER_NAME_OF_OWNER] << "#{Etc.getpwuid(status.uid).name}  #{Etc.getgrgid(status.gid).name}"
 
-  cols[3] << status.size.to_s
+  cols[FILE_GROUP_NAME_OF_OWNER] << status.size.to_s
 
   mtime = status.mtime
-  cols[4] << ((Time.now - mtime).quo(86_400) >= 182 ? mtime.strftime('%_m %_d  %Y') : mtime.strftime('%_m %_d %R'))
+  cols[FILE_FILE_BYTE_SIZE] << ((Time.now - mtime).quo(86_400) >= 182 ? mtime.strftime('%_m %_d  %Y') : mtime.strftime('%_m %_d %R'))
 
-  cols[5] << (File.ftype(path) == 'link' ? "#{path} -> #{File.readlink(path)}" : path)
+  cols[FILE_LAST_MODIFIED_TIME] << (File.ftype(path) == 'link' ? "#{path} -> #{File.readlink(path)}" : path)
 
-  total_blocks += status.blocks
-
-  [total_blocks, cols]
+  { blocks: status.blocks, cols: cols }
 end
 
 # 詳細表示のレイアウト
 def parts_layout(cols)
-  result = +''
+  files = +''
 
-  (0...cols[0].length).to_a.product(cols).each do |i, col|
-    result << if cols.first == col
-                col[i]
-              elsif cols.last == col
-                " #{col[i]}"
-              else
-                margin = /\A[0-9]+\z/.match?(col[i]) ? 2 : 1
-                col[i].rjust(col.max_by(&:length).length + margin)
-              end
-    result << "\n" if col == cols.last
+  row_size = cols[0].length
+  (0...row_size).each do |i|
+    cols.each do |col|
+      files << if cols.first == col
+                 col[i]
+               elsif cols.last == col
+                 " #{col[i]}\n"
+               else
+                 margin = /\A[0-9]+\z/.match?(col[i]) ? 2 : 1
+                 col[i].rjust(col.max_by(&:length).length + margin)
+               end
+    end
   end
 
-  result
+  files
 end
 
 # 詳細表示
 def path_detail(path_list)
-  cols = Array.new(6) { [] }
+  cols = []
 
   total_blocks = 0
 
   path_list.each do |path|
-    total_blocks, cols = generate_parts(path, total_blocks, cols)
+    generate_parts = generate_parts(path)
+    total_blocks += generate_parts[:blocks]
+    cols << generate_parts[:cols]
   end
 
-  "total #{total_blocks}\n#{parts_layout(cols)}"
+  "total #{total_blocks}\n#{parts_layout(cols.transpose)}"
 end
 
 opt = OptionParser.new
